b460c9daed1ecbbb5ab90b267a517d9d8e15026e
MALLEMPUDI VENKATA PREETHI <venkatapreethimallempudi@gmail.com>
2025-08-04 10:51:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecomJenkinsPractice">
+    <BrowserRouter basename="/ecomJenkinsPactice">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
